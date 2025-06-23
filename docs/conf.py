c6daf21b863a2abcb9c39646115c2bf5faaba65a
edzillion <edzillion@gmail.com>
2025-06-23 20:59:37 +0200

    bump ver

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'Godot Stat Math'
 copyright = '2025, edzillion'
 author = 'edzillion'
-release = '0.0.4'
+release = '0.0.5'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
